8564355899787cc1d1cd634513b595119d8e3d8b
Rufus Pollock <rufus.pollock@okfn.org>
2012-05-10 01:31:57 +0100

    [conf][xs]: change name to Data Wranglers' Handbook.

--- text/conf.py
+++ text/conf.py
@@ -40,8 +40,8 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'Data Patterns'
-copyright = u'&copy; 2011, Open Knowledge Foundation'
+project = u"Data Wranglers' Handbook" 
+copyright = u'&copy; 2011-2012, Open Knowledge Foundation'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
